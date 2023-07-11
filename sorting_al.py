efeec187a2284474841f51a97512a5fc0384a5b4
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-07-11 11:29:16 +1000

    Fixing

--- sorting_al.py
+++ sorting_al.py
@@ -1,10 +1,18 @@
 import time
 import random
-nums_to_sort={}
-r1=random.randint(1,100)
-for i in range(1,r1):
-    nums_to_sort=random.randint(1,100)
+nums_to_sort=[12,24,6,8,9,64738,64,3,2,2,1]
+
     
-
+count=0
 print(nums_to_sort)
-
+smallest_num=1
+def sort(sort_function):
+    if nums_to_sort<0:
+        print(nums_to_sort)
+        nums_to_sort[1]=smallest_num
+        
+    if nums_to_sort [2]< smallest_num:
+        print ("New Smallest Number is "+str(smallest_num))
+        smallest_num= nums_to_sort [2]
+        count+1
+sorted_nums=[]
