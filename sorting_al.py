30df83738ad92dbf2f30d7b50e2eca8d10e45aa0
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-07-11 11:43:08 +1000

    =) IM CODING YEAHYEA

--- sorting_al.py
+++ sorting_al.py
@@ -1,15 +1,24 @@
 import time
 import random
-nums_to_sort=[12,24,6,8,9,64738,64,3,2,2,1]
-count=0
-print(nums_to_sort)
+running=True
 smallest_num=1
+nums_to_sort=[12,24,6,8,9,64738,64,3,2,2,1]
+
+count=(nums_to_sort.length)
+for i in range(nums_to_sort.length):
+    print("FILLER")
+
+
+
+print(nums_to_sort)
+
 def sort(sort_function):
-    if nums_to_sort<0:
+    for i in range(1):
         print(nums_to_sort)
         nums_to_sort[1]=smallest_num
-        
-    if nums_to_sort [2]< smallest_num:
+        count=2
+      
+    if nums_to_sort [count]< smallest_num:
         print ("New Smallest Number is "+str(smallest_num))
         smallest_num= nums_to_sort [2]
         count+1
