fca40b28c30a1ceb8ae400a9bb15f7700f34be45
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-07-11 13:24:40 +1000

    idk

--- sorting_al.py
+++ sorting_al.py
@@ -1,17 +1,17 @@
 import time
 import random
-running=True
+sorted=[]
 smallest_num=1
 nums_to_sort=[12,24,6,8,9,64738,64,3,2,2,1]
 smallest=nums_to_sort[0]
 pos=0
 count=1
-for i in range(len(nums_to_sort)):
-    if nums_to_sort[i]<smallest:
-        smallest=nums_to_sort[i]
-        pos=[i]
-
-del nums_to_sort [pos]
+while nums_to_sort:
+    for i in range(len(nums_to_sort)):
+        if nums_to_sort[i]<smallest:
+            smallest=nums_to_sort[i]
+            pos=i
+            del nums_to_sort [pos]
 print(i)
 print(nums_to_sort)
 
