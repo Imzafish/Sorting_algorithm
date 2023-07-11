4ac53b5bb2fd045728a0aeb530ddf1aac0eae846
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-07-11 14:45:29 +1000

    eeee

---
+++ selection_sort.py
@@ -0,0 +1,19 @@
+import time
+import random
+sorted_list=[]
+nums_to_sort=[55,56, 57,57,55,64,64,3,2,1,3]
+
+
+while nums_to_sort:
+    smallest = nums_to_sort[0]
+    pos = 0
+    for i in range(len(nums_to_sort)):
+        if nums_to_sort[i]<smallest:
+            smallest=nums_to_sort[i]
+            pos=i
+            
+    del nums_to_sort [pos]
+    sorted_list.append(smallest)
+print(sorted_list)
+
+
