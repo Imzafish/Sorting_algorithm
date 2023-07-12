e3cd35dd5040d9355acc6b75346fa5527e072d9f
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-07-12 11:19:17 +1000

    count

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,7 +3,6 @@ import random
 start=time.time()
 sorted_list=[]
 nums_to_sort=[55,56, 57,57,55,64,64,3,2,1,3]
-count=0
 print("Sorting beginning...")
 nums_to_sort_length=len(nums_to_sort)
 for i in range(nums_to_sort_length):                           
