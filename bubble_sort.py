414a7273fcbe41f65812699e76d282f92a97f1fa
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-07-12 11:53:07 +1000

    finished bubble sort me failed hard tho took a while

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,16 +1,14 @@
-import time
 import random
+import time
 start=time.time()
-sorted_list=[]
-nums_to_sort=[55,56, 57,57,55,64,64,3,2,1,3]
-print("Sorting beginning...")
-nums_to_sort_length=len(nums_to_sort)
-for i in range(nums_to_sort_length):                           
-    if nums_to_sort[i-1]<nums_to_sort[i]:
-        smallest_num=nums_to_sort[i]
-        sorted_list.append(smallest_num)       
-    
-
-print("Sorting ended..."+"\n Sorted list: "+str(sorted_list))
+nums=[55,56, 57,57,55,64,64,3,2,1,3]
+print ("Sorting Nums...")
+for a in range(len(nums)):
+    for i in range(len(nums)-1):
+        if nums[i] > nums[i+1]:
+            temp = nums[i]
+            nums[i] = nums[i+1]
+            nums[i+1] = temp
+print("Sorting ended..."+"\n Sorted list: "+str(nums))
 end=time.time()
 print("Time to sort is " + str(end-start))
