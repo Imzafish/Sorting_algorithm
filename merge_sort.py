fa63b1c6e4f1181213bb26775c5b358b95e8312d
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-07-12 13:39:42 +1000

    function

--- merge_sort.py
+++ merge_sort.py
@@ -5,13 +5,20 @@ nums=[55,56, 57,57,55,64,64,3,2,1,3]
 half = len(nums) // 2
 first_half = nums[:half]
 second_half = nums[half:]
-
 print("First half:", first_half)
 print("Second half:", second_half)
-crunch=len(first_half) // 2
-crunch_1= first_half[:crunch]
-crunch_2= second_half[:crunch]
-print("Crunch 1:", crunch_1)
-print("Crunch 2:", crunch_2)
+
+def split():
+    crunch=len(first_half) // 2
+    crunch_1= first_half[:crunch]
+    crunch_2= second_half[:crunch]
+    print("First Half:", crunch_1)
+    print("Second Half:", crunch_2)
+
+crunchy=len(second_half) // 2
+crunch_A= first_half[:crunchy]
+crunch_B= second_half[:crunchy]
+print("Crunchy 1:", crunch_A)
+print("Crunchy 2:", crunch_B)
 
 
