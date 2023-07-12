bc8d20c4f5a66577e486f56509796d0e2c37ef97
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-07-12 13:12:31 +1000

    Halfed numbers now need to crunch sepratelty!

--- merge_sort.py
+++ merge_sort.py
@@ -1,5 +1,10 @@
 import time
 import random
+
 nums=[55,56, 57,57,55,64,64,3,2,1,3]
-Num_halved=len(nums)/2
-print ("Num_halved")
+half = len(nums) // 2
+first_half = nums[:half]
+second_half = nums[half:]
+
+print("First half:", first_half)
+print("Second half:", second_half)
